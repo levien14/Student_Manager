09290b5432fecf9d5b5a63876a739a06bec8a559
LeVien <vienlvv14091998@gmail.com>
2019-01-04 21:53:44 +0700

    Khoi tao git

---
+++ Client/StudentManager/Data/StudentManagerContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentManager.Models
+{
+    public class StudentManagerContext : DbContext
+    {
+        public StudentManagerContext (DbContextOptions<StudentManagerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentManager.Models.Account> Account { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Grade>()
+                .HasIndex(u => u.GradeName)
+                .IsUnique();
+        }
+        public DbSet<StudentManager.Models.Person> Person { get; set; }
+        public DbSet<StudentManager.Models.Subject> Subject { get; set; }
+        public DbSet<StudentManager.Models.Grade> Grade { get; set; }
+        public DbSet<StudentManager.Models.Mark> Mark { get; set; }
+        public DbSet<StudentManager.Models.GradeStudent> GradeStudent { get; set; }
+        public DbSet<StudentManager.Models.SubjectGrade> SubjectGrade { get; set; }
+        public DbSet<StudentManager.Models.Role> Role { get; set; }
+        public DbSet<StudentManager.Models.AccountRole> AccountRole { get; set; }
+    }
+}
